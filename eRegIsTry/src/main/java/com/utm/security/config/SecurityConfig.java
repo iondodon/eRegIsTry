f57cefc4f4d8a8e7a3719a404fe5e60b14406627
Ion Dodon <iondodon2@gmail.com>
2019-11-20 11:34:02 +0200

    combining with dev

--- eRegIsTry/src/main/java/com/utm/security/config/SecurityConfig.java
+++ eRegIsTry/src/main/java/com/utm/security/config/SecurityConfig.java
@@ -25,7 +25,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected  void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().authenticated()
                 .antMatchers("/home").hasRole("USER")
                 .antMatchers("/my-account").hasRole("USER")
 
