589cf4c907162a7e1a43b8f0485c52c809c04d90
Ion Dodon <iondodon2@gmail.com>
2019-11-20 11:04:57 +0200

    combining with dev

--- eRegIsTry/src/main/java/com/utm/controllers/LoginController.java
+++ eRegIsTry/src/main/java/com/utm/controllers/LoginController.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class SecurityController {
+public class LoginController {
     @GetMapping("/showLoginPage")
     public String showLoginPage() {
 
